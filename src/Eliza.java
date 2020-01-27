437d1852c4ccd6297d2eef183e59175be9346822
GBTC441011UR <GBTC441011UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-01-27 16:02:43 -0500

    replace first person with second

--- src/Eliza.java
+++ src/Eliza.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Eliza {
@@ -9,12 +10,28 @@ public class Eliza {
         while(!again) {
             System.out.print("Good day. What is your problem? Enter your response here or Q to quit: ");
             input = sc.nextLine();
-            System.out.println(input);
 
+
+            String[] inputArray = input.split(" ");
+
+            for(int i = 0; i < inputArray.length; i++){
+                if(inputArray[i].equalsIgnoreCase("i") || inputArray[i].equalsIgnoreCase("me")){
+                    inputArray[i] = "you";
+                } else if (inputArray[i].equalsIgnoreCase("my")){
+                    inputArray[i] = "your";
+                }else if(inputArray[i].equalsIgnoreCase("am")){
+                    inputArray[i] = "are";
+                }
+
+            }
             if(input.equalsIgnoreCase("q") || input.equalsIgnoreCase("I am feeling great" )){
                 System.out.println(">>> END");
                 break;
             }
+
+            System.out.println("Why do you say that " + String.join("  ", inputArray) + "?");
+
+
         }
     }
 }
