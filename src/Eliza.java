65f4e82423e524b65593293d382a1a11965394a7
GBTC441011UR <GBTC441011UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-01-27 16:36:22 -0500

    adding ramdoness

--- src/Eliza.java
+++ src/Eliza.java
@@ -1,14 +1,27 @@
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Eliza {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input;
+        String[] hedges = {"Please tell me more ","Many of my patients tell me the same thing ","It is getting late, maybe we had better quit "};
+        String[] qualifiers = {"Why do you say that ","So, you are concerned that "};
+
+        Random r = new Random();
+
+
 
         Boolean again = false;
+        System.out.print("Good day. What is your problem? Enter your response here or Q to quit: ");
         while(!again) {
-            System.out.print("Good day. What is your problem? Enter your response here or Q to quit: ");
+            int x = r.nextInt(3);
+            int y = r.nextInt(2);
+            String randomHedge = hedges[x];
+            String randomQualifiers = qualifiers[y];
+
+
             input = sc.nextLine();
 
 
@@ -29,7 +42,9 @@ public class Eliza {
                 break;
             }
 
-            System.out.println("Why do you say that " + String.join("  ", inputArray) + "?");
+            System.out.println(randomQualifiers + String.join("  ", inputArray) + "?");
+            System.out.println(randomHedge);
+            System.out.print("Enter your response here or Q to quit: ");
 
 
         }
