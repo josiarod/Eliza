1d22541d906411ec0a87eb00a616b1209752db0e
GBTC441011UR <GBTC441011UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-01-28 10:54:29 -0500

    created method

--- src/Eliza.java
+++ src/Eliza.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,11 +7,7 @@ public class Eliza {
         String input;
         String[] hedges = {"Please tell me more ","Many of my patients tell me the same thing ","It is getting late, maybe we had better quit "};
         String[] qualifiers = {"Why do you say that ","So, you are concerned that "};
-
         Random r = new Random();
-
-
-
         Boolean again = false;
         System.out.print("Good day. What is your problem? Enter your response here or Q to quit: ");
         while(!again) {
@@ -20,33 +15,77 @@ public class Eliza {
             int y = r.nextInt(2);
             String randomHedge = hedges[x];
             String randomQualifiers = qualifiers[y];
-
-
             input = sc.nextLine();
 
 
-            String[] inputArray = input.split(" ");
 
-            for(int i = 0; i < inputArray.length; i++){
-                if(inputArray[i].equalsIgnoreCase("i") || inputArray[i].equalsIgnoreCase("me")){
-                    inputArray[i] = "you";
-                } else if (inputArray[i].equalsIgnoreCase("my")){
-                    inputArray[i] = "your";
-                }else if(inputArray[i].equalsIgnoreCase("am")){
-                    inputArray[i] = "are";
-                }
 
-            }
+
             if(input.equalsIgnoreCase("q") || input.equalsIgnoreCase("I am feeling great" )){
                 System.out.println(">>> END");
                 break;
             }
 
-            System.out.println(randomQualifiers + String.join("  ", inputArray) + "?");
+            if(input.equalsIgnoreCase("pig")){
+                String[] inputArray = input.split(" ");
+                for(int i = 0; i < inputArray.length; i++){
+                   solve(inputArray[i]);
+                }
+                System.out.println();
+            }
+
+
+            System.out.println(randomQualifiers + String.join("  ", question(input)) + "?");
             System.out.println(randomHedge);
             System.out.print("Enter your response here or Q to quit: ");
 
 
         }
     }
-}
+
+    public static String[] question(String input){
+
+        String[] inputArray = input.split(" ");
+        for(int i = 0; i < inputArray.length; i++){
+
+            if(inputArray[i].equalsIgnoreCase("i") || inputArray[i].equalsIgnoreCase("me")){
+                inputArray[i] = "you";
+            } else if (inputArray[i].equalsIgnoreCase("my")){
+                inputArray[i] = "your";
+            }else if(inputArray[i].equalsIgnoreCase("am")){
+                inputArray[i] = "are";
+            }
+        }
+
+               return inputArray;
+    }
+
+
+
+
+    public static String solve (String word) {
+        String temp = word.toLowerCase();
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+        char first = temp.charAt(0);
+
+
+        for (int i = 0; i < vowels.length; i++) {
+            if (first == vowels[i]) {
+                return word + "way";
+            }
+        }
+
+        word = word.substring(1);
+        word += first + "ay";
+
+
+        return word;
+    }
+
+
+
+
+    }
+
+
+
